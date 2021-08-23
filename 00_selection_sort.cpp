1c8cc71ce0e64d17b152e60812f576f0e160043e
nevikw39 <nevikw39@icloud.com>
2021-08-23 11:20:08 +0800

    Replace naive sort to selection sort

--- 00_selection_sort.cpp
+++ 00_selection_sort.cpp
@@ -4,18 +4,16 @@
 
 using namespace std;
 
-void naive_sort(vector<int> &v)
+void selection_sort(vector<int> &v)
 {
-    vector<int> tp(v.size()); // temporarily store our result
-    for (int i = 0, n = v.size(); i < n; i++)
+    for (int i = 0, n = v.size(); i < n - 1; i++)
     {
-        int min = INT_MAX;                        // initialize current minimum to infinity
-        for (const int &j : v)                    // for all j in v
-            if (min > j && (!i || j > tp[i - 1])) // if minimum > j and (i isn't the first or j > prevois minimum)
-                min = j;
-        tp[i] = min;
+        int index_of_min = i;
+        for (int j = i + 1; j < n; j++)
+            if (v[index_of_min] > v[j])
+                index_of_min = j;
+        swap(v[i], v[index_of_min]);
     }
-    v = tp;
 }
 
 int main()
@@ -29,7 +27,7 @@ int main()
     cout << "Before sorting:\n";
     for (const int &i : v)
         cout << i << ' ';
-    naive_sort(v);
+    selection_sort(v);
     cout << "\n\nAfter sorting:\n";
     for (const int &i : v)
         cout << i << ' ';
