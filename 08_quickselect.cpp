a1ff256b4bc2bd186bb0dc15ba2a1dea198d59f3
nevikw39 <nevikw39@icloud.com>
2021-08-24 17:02:33 +0800

    quickselect

---
+++ 08_quickselect.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <random> // for mt19937
+
+using namespace std;
+
+int quickselect(vector<int>::iterator begin, vector<int>::iterator end, int k)
+{
+    if (end - begin <= 1)
+        return *begin;
+    auto itr = begin, pivot = end - 1;
+    for (auto jtr = begin; jtr != pivot; jtr++)
+        if (*jtr < *pivot)
+            swap(*itr++, *jtr);
+    swap(*itr, *pivot);
+    if (k < itr - begin)
+        return quickselect(begin, itr, k);
+    else if (k == itr - begin)
+        return *itr;
+    else                                                         // the size of less aprt is (itr - begin) ans pivot alse counts
+        return quickselect(itr + 1, end, k - (itr - begin) - 1); // so the k-th in origin is (k - (itr - begin) - 1) in greater part
+}
+
+int main()
+{
+    vector<int> v = {8, 27, 2021, 110, 20};
+    cout << "v:";
+    for (const int &i : v)
+        cout << ' ' << i;
+    cout << '\n'
+         << quickselect(v.begin(), v.end(), 2) << '\n';
+    int n, k;
+    cin >> n >> k;
+    v.resize(n);
+    mt19937 _rand((random_device())()); // create an object with random seed that generates random numbers using Mersenne Twister Algo
+    for (int &i : v)
+        i = _rand();
+    cout << "v:";
+    for (const int &i : v)
+        cout << ' ' << i;
+    cout << '\n'
+         << quickselect(v.begin(), v.end(), k) << '\n';
+    return 0;
+}
---
+++ 09_leetcode0215.cpp
@@ -0,0 +1,49 @@
+#if !__clang__ || __clang_major__ >= 12
+#include <iostream>
+#include <vector>
+#include <random> // for mt19937
+using namespace std;
+#endif
+
+mt19937 _rand((random_device())()); // create an object with random seed that generates random numbers using Mersenne Twister Algo
+
+class Solution
+{
+private:
+    int quickselect(vector<int>::iterator begin, vector<int>::iterator end, int k)
+    {
+        int n = end - begin;
+        if (n <= 1)
+            return *begin;
+        auto itr = begin, pivot = begin + (_rand() % n + n) % n; // randomly pick pivot
+        swap(*pivot, *(end - 1));                                // swap random pivot to last so that we won't process it
+        pivot = end - 1;                                         // update pivot to its exact place
+        for (auto jtr = begin; jtr != pivot; jtr++)
+            if (*jtr < *pivot)
+                swap(*itr++, *jtr);
+        swap(*itr, *pivot);
+        if (k < itr - begin)
+            return quickselect(begin, itr, k);
+        else if (k == itr - begin)
+            return *itr;
+        else                                                         // the size of less aprt is (itr - begin) ans pivot alse counts
+            return quickselect(itr + 1, end, k - (itr - begin) - 1); // so the k-th in origin is (k - (itr - begin) - 1) in greater part
+    }
+
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        return quickselect(nums.begin(), nums.end(), nums.size() - k);
+    }
+};
+
+#if !__clang__ || __clang_major__ >= 12
+int main()
+{
+    vector<int> a = {3, 2, 1, 5, 6, 4}, b = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+    Solution sln;
+    cout << sln.findKthLargest(a, 2) << '\n'
+         << sln.findKthLargest(b, 4) << '\n';
+    return 0;
+}
+#endif
