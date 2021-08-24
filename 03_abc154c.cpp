ef7d3096c0c5231b0d69a22d59da2327c03a1785
nevikw39 <nevikw39@icloud.com>
2021-08-24 14:09:26 +0800

    AtCoder Beginner Contest 154 p. C - Distinct or Not

---
+++ 03_abc154c.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <random> // for mt19937
+
+using namespace std;
+
+bool merge_sort(vector<int>::iterator begin, vector<int>::iterator end)
+{
+    // 0. recursion boundary
+    if (end - begin == 1)
+        return true;
+    // 1. Divide
+    auto mid = begin + ((end - begin) >> 1); // mid = (begin + end) / 2
+    vector<int> left(begin, mid), right(mid, end);
+    // 2. Conquer
+    if (!merge_sort(left.begin(), left.end()) || !merge_sort(right.begin(), right.end()))
+        return false;
+    // 3. Combine
+    for (auto itr = left.begin(), jtr = right.begin(); begin != end; begin++)
+        if (itr != left.end() && jtr != right.end())
+        {
+            if (*itr < *jtr)
+                *begin = *itr++;
+            else if (*itr == *jtr)
+                return false;
+            else
+                *begin = *jtr++;
+        }
+        else if (itr != left.end())
+            *begin = *itr++;
+        else
+            *begin = *jtr++;
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int &i : v)
+        cin >> i;
+    cout << (merge_sort(v.begin(), v.end()) ? "YES\n" : "NO\n");
+    return 0;
+}
